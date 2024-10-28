dfb9582cd81aca9d2642734a8c198b9050053add
Kubanych Asanov <kuba.asanov4@gmail.com>
2024-10-28 15:06:51 +0600

    Настроил локализацию

---
+++ lib/src/localization/generated/app_localization_ky.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: always_use_package_imports, unnecessary_string_escapes
+
+import 'app_localization.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Kirghiz Kyrgyz (`ky`).
+class AppLocalizationsKy extends AppLocalizations {
+  AppLocalizationsKy([String locale = 'ky']) : super(locale);
+
+  @override
+  String get appTitle => 'surapp_flutter';
+}
---
+++ lib/src/localization/generated/app_localization_ru.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: always_use_package_imports, unnecessary_string_escapes
+
+import 'app_localization.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Russian (`ru`).
+class AppLocalizationsRu extends AppLocalizations {
+  AppLocalizationsRu([String locale = 'ru']) : super(locale);
+
+  @override
+  String get appTitle => 'surapp_flutter';
+}
