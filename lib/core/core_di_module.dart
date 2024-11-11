821324141658307f51bc3a6dcaab90dd39d7b869
Kubanych Asanov <kuba.asanov4@gmail.com>
2024-11-11 14:35:22 +0600

    Added rest client

--- lib/core/core_di_module.dart
+++ lib/core/core_di_module.dart
@@ -1,11 +1,22 @@
+import 'package:alice/alice.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:surapp_flutter/common/app_urls.dart';
+import 'package:surapp_flutter/common/network/auth_rest_client.dart';
+import 'package:surapp_flutter/common/network/locale_interceptor.dart';
+import 'package:surapp_flutter/common/network/unauth_rest_client.dart';
 import 'package:surapp_flutter/core/storage/local_storage.dart';
 import 'package:surapp_flutter/core/storage/local_storage_impl.dart';
+import 'package:surapp_flutter/core/storage/secure_storage.dart';
+import 'package:surapp_flutter/core/storage/secure_storage_impl.dart';
 import 'package:surapp_flutter/features/settings/domain/usecases/update_locale_usecase.dart';
 import 'package:surapp_flutter/features/settings/domain/usecases/update_theme_usecase.dart';
 import 'package:surapp_flutter/features/settings/presentation/bloc/settings_bloc.dart';
 import 'package:surapp_flutter/features/settings/data/settings_service.dart';
 import 'package:take_it/take_it.dart';
+
+const networkTimeout = Duration(seconds: 60);
 
 class CoreDiModule extends DiModuleAsync {
   @override
@@ -16,23 +27,70 @@ class CoreDiModule extends DiModuleAsync {
       ..registerSingleton<LocalStorage>(
         LocalStorageImpl(sharedPreferences),
       )
-      // Data
-      //
-      ..registerSingleton<SettingsService>(
-          SettingsService(localStorage: get<LocalStorage>()))
+      ..registerSingleton<SecureStorage>(
+        SecureStorageImpl(),
+      )
 
-      // Domain
-      //
+      /// Data
+      ///
+      ..registerSingleton<SettingsService>(
+          SettingsService(localStorage: get<LocalStorage>()));
+
+    final themeMode = await get<SettingsService>().themeMode();
+    final locale = await get<SettingsService>().locale();
+
+    /// Network
+    ///
+    it
+      ..registerSingleton(Alice(
+        showNotification: false,
+        showInspectorOnShake: true,
+      ))
+      ..registerFactory(() {
+        final dio = Dio(BaseOptions(
+          baseUrl: AppUrls.baseUrl,
+          receiveTimeout: networkTimeout,
+          connectTimeout: networkTimeout,
+        ));
+        dio.interceptors.addAll([
+          LocaleInterceptor(
+              localeGetter: () => locale.languageCode.toUpperCase()),
+          get<Alice>().getDioInterceptor(),
+        ]);
+        if (kDebugMode) {
+          dio.interceptors.add(
+            LogInterceptor(
+              responseBody: true,
+              requestBody: true,
+              logPrint: (object) {
+                final pattern = RegExp('.{1,800}');
+
+                pattern
+                    .allMatches(object.toString())
+                    .forEach((match) => debugPrint(match.group(0)));
+              },
+            ),
+          );
+        }
+        return dio;
+      })
+      ..registerSingleton(AuthRestClient(
+        get<Dio>(),
+        get<SecureStorage>(),
+      ))
+      ..registerSingleton(UnAuthRestClient(
+        get<Dio>(),
+      ))
+
+      /// Domain
+      ///
       ..registerFactory<UpdateThemeUsecase>(
           () => UpdateThemeUsecase(get<SettingsService>()))
       ..registerFactory<UpdateLocaleUsecase>(
           () => UpdateLocaleUsecase(get<SettingsService>()));
 
-    // Presentation
-    //
-    final themeMode = await get<SettingsService>().themeMode();
-    final locale = await get<SettingsService>().locale();
-
+    /// Presentation
+    ///
     it.registerLazySingleton<SettingsBloc>(
       () {
         return SettingsBloc(
